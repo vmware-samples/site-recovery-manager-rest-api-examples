849299d5415bae4f1a07a9d4abe116cb8b15d2a7
sstych <sstoychev@vmware.com>
2023-07-26 19:39:43 +0300

    Initial repository setup
    Signed-off-by: Stefan Stoychev  <sstoychev@vmware.com>

---
+++ java/vrms-appliance-config/code-examples/src/main/java/dr/restapi/examples/vrms/config/MyFirstExample.java
@@ -0,0 +1,6 @@
+/* Copyright 2023 VMware, Inc. */
+/* SPDX-License-Identifier: BSD-2-Clause */
+package dr.restapi.examples.vrms.config;
+
+public class MyFirstExample {
+}
---
+++ java/vrs-appliance-config/code-examples/src/main/java/dr/restapi/examples/vrs/config/MyFirstExample.java
@@ -0,0 +1,6 @@
+/* Copyright 2023 VMware, Inc. */
+/* SPDX-License-Identifier: BSD-2-Clause */
+package dr.restapi.examples.vrs.config;
+
+public class MyFirstExample {
+}
